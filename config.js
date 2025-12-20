9fe6fb335dc8931025a231812ece29e940601a3f
vtphonghth <vtphonghth@gmail.com>
2025-12-20 08:09:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,10 @@
 
 // URL của Google Apps Script đã deploy
 // Thay đổi URL này khi deploy script mới
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyVkJJVgJYZSjlBR_W48Vkix2vx4v1RRRBnpe-tfQksk-uuJhGZayZML3U7Dgv9jS4WUA/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzWMmFrdEm7ABAg8xwWiHrDrrW8jreRUQtAnvFH9h9FTvXXhEViHTkoCZNqy9gsJqPSTQ/exec';
 
 // Spreadsheet ID (nếu cần sử dụng trong frontend)
 // ID này được sử dụng trong unified.gs
 const SPREADSHEET_ID = '1YKNvKQoaYR2-OV1k6JvZlmPF63GdMPacuw602LKPtx8';
+
 
